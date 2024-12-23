595e261f39e7c7b36ebd537f1424e746b8fb33e8
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2024-12-22 23:19:02 -0300

    Captulo 2 parte 5

--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,15 @@
+import "dotenv/config";
 import { Knex, knex as setupKnex } from "knex";
 
+if(!process.env.DATABASE_URL){
+    throw new Error("No DATABASE_URL environment variable found.");
+}
 
+console.log(process.env);
 export const config: Knex.Config = {
     client: "sqlite",
     connection: {
-        filename: "./db/database.sqlite",
+        filename: process.env.DATABASE_URL,
     },
     useNullAsDefault: true,
     migrations : {
