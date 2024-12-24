88bc8663d79b00490e04e3af33695a5902044c8a
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2024-12-24 16:28:44 -0300

    Capitulo 4 parte 6

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -49,4 +49,60 @@ describe("Transaction routes", () => {
             })
         ]);
     });
+
+    it("should be able to get specific transaction", async () => {
+        const createResponse = await supertest(app.server)
+            .post("/transactions")
+            .send({
+                title: "New transaction",
+                amount: 5000,
+                type: "credit"
+            });
+
+        const cookies = createResponse.get("Set-Cookie")!;
+        const listResponse = await supertest(app.server)
+            .get("/transactions")
+            .set("Cookie", cookies)
+            .expect(200);
+
+
+        const transactionId = listResponse.body.data[0].id;
+
+        const transactionResponse = await supertest(app.server).get(`/transactions/${transactionId}`).set("Cookie", cookies).expect(200);
+
+        expect(transactionResponse.body.data).toEqual(
+            expect.objectContaining({
+                title: "New transaction",
+                amount: 5000
+            })
+        );
+    });
+
+    it("should be able to get the sumary", async () => {
+        const createResponse = await supertest(app.server)
+            .post("/transactions")
+            .send({
+                title: "New transaction",
+                amount: 5000,
+                type: "credit"
+            });
+
+        const cookies = createResponse.get("Set-Cookie")!;
+
+        await supertest(app.server)
+            .post("/transactions")
+            .set("Cookie",cookies)
+            .send({
+                title: "Debit transaction",
+                amount: 2000,
+                type: "debit"
+            });
+
+        const summaryResponse = await supertest(app.server)
+            .get("/transactions/summary")
+            .set("Cookie", cookies)
+            .expect(200);
+
+        expect(summaryResponse.body.summary).toEqual({amount: 3000 });
+    });
 });
